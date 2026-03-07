27bfaeb91f0f0a321fda9b8f558d24c0e48dcada
Herzan <46695573+Herzan@users.noreply.github.com>
2026-03-06 22:56:09 -0600

    d

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/users', require('./users'));
+
 router.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Welcome to Project 1 API');
 });
 
 module.exports = router;
