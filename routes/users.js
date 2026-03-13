d540ac257f1d1114bb24175df704a7f47e05c43c
Herzan <46695573+Herzan@users.noreply.github.com>
2026-03-13 17:38:29 -0600

    herzan

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,14 @@ router.get('/', usersController.getAll);
 
 router.get('/:id', usersController.getSingle);
 
+router.post('/', usersController.createUser);
+
+router.put('/:id', usersController.updateUser);
+
+router.delete('/:id', usersController.deleteUser);
+
 module.exports = router;
+
+
+//https://project1-8r7q.onrender.com/
+//https://github.com/Herzan/CSE-341-project1
